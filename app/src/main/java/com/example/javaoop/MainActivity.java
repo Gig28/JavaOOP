1558ab9fedff36f68d5d474ec01977ef1cb2e8bc
Gig28 <128386775+Gig28@users.noreply.github.com>
2023-10-02 23:22:40 +0300

    Приведение типов

--- app/src/main/java/com/example/javaoop/MainActivity.java
+++ app/src/main/java/com/example/javaoop/MainActivity.java
@@ -6,6 +6,8 @@ import android.annotation.SuppressLint;
 import android.widget.TextView;
 import android.os.Bundle;
 import android.util.Log;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     Cat myCat;
     @Override
@@ -14,26 +16,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Cat vaska = new Cat();
-        vaska.talk(3);
+
 
         Puma bagira = new Puma();
-        bagira.talk();
+
 
         Cat puma = new Puma();
-        puma.talk();
+
 
         Cat murzik = new Lion();
-        murzik.talk();
+
+        Lion lion = new Lion();
+
+
+        ArrayList<Cat> cats = new ArrayList<>();
+        cats.add(vaska);
+        cats.add(bagira);
+        cats.add(puma);
+        cats.add(murzik);
+        cats.add(lion);
+
+
+        Cat cat = cats.get(0);
+        Cat cat1 = cats.get(1);
+        Cat cat2 = cats.get(2);
+        Cat cat3 = cats.get(3);
+        Cat cat4 = cats.get(5);
+
+
+        Puma puma1 = (Puma)cats.get(1);
+        puma1.talk();
+
+        Cat cat5 = cats.get(4);
+
+        if(cat5 instanceof Puma){
+            Puma puma2 = (Puma)cat5;
+            puma2.talk();
+            TextView textView3 = (TextView) findViewById(R.id.textView);
+            textView3.setText(puma2.talk());
+        }else{
+            TextView textView3 = (TextView) findViewById(R.id.textView);
+            textView3.setText("This is not Puma");
+            Log.i("instanceof", "This is not Puma");
+        }
+
+
 
 
         TextView textView2 = (TextView) findViewById(R.id.textView);
-        textView2.setText(bagira.talk());
+        textView2.setText(puma1.talk());
 
-        TextView textView3 = (TextView) findViewById(R.id.textView2);
-        textView3.setText(puma.talk());
 
-        TextView textView4 = (TextView) findViewById(R.id.textView3);
-        textView4.setText(murzik.talk());
+
+
 
 //        myCat = new Cat(4, "Puma","Бенгальская","Серая");
 //        myCat.talk();
